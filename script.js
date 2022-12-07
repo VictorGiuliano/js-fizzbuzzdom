8057ce6475ed07a8cedacdbe939816af7369a8d2
VictorGiuliano <victorgiuliano98@gmail.com>
2022-12-07 19:14:30 +0100

    Inserimento messaggio Fizz e Buzz

--- script.js
+++ script.js
@@ -11,8 +11,17 @@ per i multipli di 5 stampi “Buzz” al posto del numero.
 
 //1.
  const targetElement = document.getElementById('target');
-
+ let j;
+ let y;
  //2.
  for (let i=1; i<=100; i++){
-    console.log(i);
+    j = y = i;
+    if (j % 3 === 0){
+        console.log('Fizz');
+    }else if (y % 5 === 0){
+        console.log('Buzz');
+    
+    }else{
+        console.log(i);
+    }
  }
