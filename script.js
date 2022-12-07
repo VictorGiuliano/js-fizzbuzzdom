d928395fb2cc361abb56c4081d9620d4b1ab495e
VictorGiuliano <victorgiuliano98@gmail.com>
2022-12-07 18:28:50 +0100

    Ciclo for per i 100 numeri

---
+++ script.js
@@ -0,0 +1,18 @@
+/*  Scrivi un programma che stampi in console i numeri da 1 a 100, ma che
+ per i multipli di 3 stampi “Fizz” al posto del numero
+per i multipli di 5 stampi “Buzz” al posto del numero.
+ Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero. */
+
+ /*
+ 1. Prendo gli elementi da Index
+ 2. Mettere un ciclo che faccia scorrere i numeri da 1 a 100
+ 3. Stampare il risultato differenziando tra i multipli di 3 e quelli di 5 */
+
+
+//1.
+ const targetElement = document.getElementById('target');
+
+ //2.
+ for (let i=1; i<=100; i++){
+    console.log(i);
+ }
